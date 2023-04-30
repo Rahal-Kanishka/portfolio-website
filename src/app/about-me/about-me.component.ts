3b84a41111b26de790887e6dee8b36981e35a755
Rahal Kanishka <rahalncm@gmail.com>
2023-04-29 23:26:59 -0400

    included about me section

---
+++ src/app/about-me/about-me.component.ts
@@ -0,0 +1,28 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+declare var anime: any;
+
+
+
+@Component({
+  selector: 'app-about-me',
+  templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.scss']
+})
+export class AboutMeComponent implements OnInit {
+
+  start: boolean = false;
+  count = 1;
+  type_speed = 60
+  type_delay = 100
+
+  ngOnInit(): void {
+    setTimeout(() => this.start = true, 1000)
+    setInterval(() => this.incrementCount(), 4000)
+  }
+
+  incrementCount() {
+    this.count = this.count % 5;
+    this.count++;
+  }
+
+}
