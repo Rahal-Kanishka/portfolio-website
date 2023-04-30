de10a5a375838ba1a589e229f107165be20c7028
Rahal Kanishka <rahalncm@gmail.com>
2023-04-29 23:28:34 -0400

    removed the default content and added portfolio page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-website';
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior: 'smooth', block: 'center', inline: 'end'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TypingAnimationModule } from "angular-typing-animation";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TypingAnimationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
