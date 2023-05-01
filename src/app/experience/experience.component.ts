f08eb4b4c7e81f21fefeb5ea00b32233d77bbb50
Rahal Kanishka <rahalncm@gmail.com>
2023-04-30 23:15:20 -0400

    Animations added

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,16 +1,26 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { animate, query, stagger, state, style, transition, trigger } from "@angular/animations";
 
 const SMALL_DEVICE_WIDTH = 370;
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss'],
+  animations: [
+    trigger('workListAnimation', [
+      state('enter',style({transform: 'translateX(0)'})),
+      state('leave',style({transform: 'translateX(-200%)'})),
+      transition('enter => leave', animate('900ms ease-out')),
+      transition('leave => enter', animate('1200ms ease-in'))
+    ])
+  ]
 })
 export class ExperienceComponent implements OnInit {
 
   constructor() { }
 
   public innerWidth: any;
+  public movement :any;
   smallDevice = false;
   selected = 1;
   ngOnInit(): void {
@@ -22,6 +32,20 @@ export class ExperienceComponent implements OnInit {
     this.innerWidth = window.innerWidth;
     this.smallDevice = window.innerWidth <= SMALL_DEVICE_WIDTH;
     console.log('width: ', this.innerWidth);
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    if (window.scrollY > 100) {
+      this.movement = 'enter'
+    } else {
+      this.movement = 'leave'
+    }
+
+    if (window.scrollY > 1000) {
+      this.movement = 'leave'
+    }
+    console.log('scroll: ', window.scrollY);
   }
 
 }
