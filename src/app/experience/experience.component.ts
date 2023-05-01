6e955534c04960abe4b3c6031c1ed7b3fa0f2263
Rahal Kanishka <rahalncm@gmail.com>
2023-04-30 21:32:30 -0400

    content added and style changes on the experience section

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
+const SMALL_DEVICE_WIDTH = 370;
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
@@ -9,7 +10,18 @@ export class ExperienceComponent implements OnInit {
 
   constructor() { }
 
+  public innerWidth: any;
+  smallDevice = false;
+  selected = 1;
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    this.smallDevice = window.innerWidth <= SMALL_DEVICE_WIDTH;
+    console.log('width: ', this.innerWidth);
   }
 
 }
