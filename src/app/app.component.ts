1499ea4d10e60247b3159de436d18ca7cdfba3f0
Rahal Kanishka <rahalncm@gmail.com>
2023-05-07 12:31:41 -0400

    includedauto select menu item based on the current position of the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,22 +17,54 @@ export class AppComponent implements OnInit{
   }
 
   selectorFunction() {
+
+    let currentSectionId:any;
+    const menuElements = document.querySelectorAll('#top-nav button');
+    console.log('menu items: ', menuElements.length);
+
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          console.log(entry.target.classList)
           entry.target.classList.add('show');
         } else {
           entry.target.classList.remove('show');
         }
       });
-    })
+    });
+
+    const sectionObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          currentSectionId = (entry.target && entry.target.getAttribute('id')) ? entry.target.getAttribute('id') : '';
+          console.log('currentSectionId: ', currentSectionId);
+
+          if (menuElements) {
+            menuElements.forEach(
+              element => {
+                if (element) {
+                  element.classList.remove('select');
+                  console.log('class list: ', element.classList);
+                  if (element && element.classList.contains(currentSectionId)) {
+                    element.classList.add('select')
+                  }
+                }
+              });
+          }
+
+        }
+      });
+    });
+
+
     const hiddenElements = document.querySelectorAll('.hidden');
     hiddenElements.forEach((el) => observer.observe(el));
 
     const furtherHiddenElements = document.querySelectorAll('.further-hidden');
     furtherHiddenElements.forEach((el) => observer.observe(el));
 
+    //sections
+    const sectionElements = document.querySelectorAll('section');
+    sectionElements.forEach((el) => sectionObserver.observe(el));
 
   }
 }
